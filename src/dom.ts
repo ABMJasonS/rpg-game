d319a23ec2cb69284f5db7a92611b925e9fe8281
Jason <118092162+ABMJasonS@users.noreply.github.com>
2024-04-15 13:25:10 -0400

    project boilerplate

---
+++ src/dom.ts
@@ -0,0 +1,32 @@
+/**
+ * A function mainly to take advantage of some code editors that
+ * syntax highlight HTML inside JS templates
+ * 
+ * @example
+ * ```ts
+ * const button = html`<button id="button-id">button content</button>`
+ * ```
+ * @param string The string to use
+ * @returns The same string inputted
+ */
+export const html = (string: TemplateStringsArray): string => string.join("");
+
+/**
+ * Selects an element from the DOM. Poor man's jQuery.
+ * @example
+ * ```ts
+ * const button = $("#button-id")
+ * ```
+ * @param selector The CSS selector
+ * @returns 
+ */
+export function $<T extends HTMLElement>(selector: string, all = false): T {
+  const element = document.querySelector(selector);
+  
+  if (element instanceof HTMLElement) {
+    // @ts-expect-error Cast the type of the element
+    return element;
+  } else {
+    throw new Error("Element not found!")
+  }
+}
---
+++ src/signals.ts
@@ -0,0 +1,83 @@
+/**
+ * A signal value that updates all it's `Derived<T>` subscribers
+ */
+export class Signal<T> {
+    private subscribers: Derived<any>[];
+    private value: T;
+
+    /**
+     * Creates a signal
+     * @param initialValue Initial value of the signal
+     */
+    constructor(initialValue: T) {
+        this.value = initialValue;
+    }
+
+    /**
+     * Sets the value of the signal and notify all its subscribers
+     * @param value Value to set
+     */
+    set(value: T) {
+        this.value = value;
+        this._notifySubscribers();
+    }
+
+    /**
+     * Changes the value of the signal and notify all its subscribers
+     * @param changeFunction The function to change the value
+     */
+    change(changeFunction: (value: T) => T) {
+        this.set(changeFunction(this.value));
+    }
+
+    /**
+     * Gets the value of the signal
+     * @returns The value of the signal
+     */
+    get(): T {
+        return this.value;
+    }
+
+    _addSubscriber(subscriber: Derived<any>) {
+        this.subscribers.push(subscriber)
+    }
+
+    _notifySubscribers() {
+        for (const subscriber of this.subscribers) {
+            subscriber._update();
+        }
+    }
+}
+
+/**
+ * A derived value that updates itself when one of it's `Signal<T>` dependencies updates
+ */
+export class Derived<T> {
+    private derivedFunction: () => T;
+    private destructor: (() => void) | undefined;
+    private value: T;
+    dependencies: Signal<any>;
+
+    /**
+     * Creates a derived value
+     * @param derivedFunction The function of the derived value
+     * @param destructor The destructor of the derived value
+     * @param dependencies The dependencies of the derived value
+     */
+    constructor(derivedFunction: () => T, destructor: (() => void) | undefined, dependencies: Signal<any>[]) {
+        this.derivedFunction = derivedFunction;
+        this.destructor = destructor;
+        this.dependencies = dependencies;
+
+        for (const dependency of dependencies) {
+            dependency._addSubscriber(this)
+        }
+
+        this._update();
+    }
+
+    _update() {
+        if (this.destructor) this.destructor();
+        this.value = this.derivedFunction();
+    }
+}
