9236c2bcb7bc281bd715ea8ea5d4e9cf046828ef
Kenos <flatware-gold-bronze@tutanota.com>
2024-04-17 10:59:38 -0600

    add pixijs

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-import { Application } from "../modules/pixi.js";
+import { Application } from "pixi.js";
+import { $ } from "./dom.js";
+
+const app = new Application();
+
+$("#main-frame").appendChild(app.canvas);
+
+app.ticker.add((ticker) => {
+  $("#player-stats").innerHTML = ticker.FPS.toString();
+});
