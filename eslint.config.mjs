ee45ffc0a9cabe99f8413c8d8a46972e1744dc3d
Kenos <flatware-gold-bronze@tutanota.com>
2024-04-18 09:38:58 -0600

    enforce code style

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,21 @@
 // @ts-expect-error shut up
 import eslint from "@eslint/js"
 import tseslint from "typescript-eslint"
+import stylistic from "@stylistic/eslint-plugin-ts"
 
 export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended
+  {
+    files: ["** /*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.recommendedTypeChecked
+    ],
+    plugins: {
+      // @ts-expect-error shut up
+      "@stylistic/ts": stylistic
+    },
+    rules: {
+      "@stylistic/ts/indent": ["error", 2]
+    }
+  }
 )
