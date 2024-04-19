689fa46d66821c33f7bd1dcc9ab991c4ad832544
Kenos <flatware-gold-bronze@tutanota.com>
2024-04-19 11:39:46 -0600

    SPINNY CAT

--- src/objects/testobject.ts
+++ src/objects/testobject.ts
@@ -7,8 +7,14 @@ export class TestObject extends GameObject {
   constructor(scene: GameScene) {
     super(createVector(0, 0), 0, scene);
     Assets.load("./img/test-image.jpg").then(image => {
-      this.pixiContainer.addChild(Sprite.from(image))
+      const sprite = Sprite.from(image)
+      sprite.anchor.set(0.5)
+      this.pixiContainer.addChild(sprite)
       this.pixiContainer.zIndex = -999
     })
+  }
+
+  override act(delta: number): void {
+    this.pixiContainer.rotation += delta * 0.3
   }
 }
