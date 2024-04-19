3365002dce6434379e6a638f47869d3c901ba305
Kenos <flatware-gold-bronze@tutanota.com>
2024-04-19 11:35:44 -0600

    add test image

--- src/objects/testobject.ts
+++ src/objects/testobject.ts
@@ -1,4 +1,4 @@
-import { Graphics } from "pixi.js";
+import { Assets, Graphics, Sprite } from "pixi.js";
 import { GameObject } from "../gameobject";
 import { GameScene } from "../scene";
 import { createVector } from "../vector";
@@ -6,10 +6,9 @@ import { createVector } from "../vector";
 export class TestObject extends GameObject {
   constructor(scene: GameScene) {
     super(createVector(0, 0), 0, scene);
-    this.pixiContainer.addChild(
-      new Graphics({ pivot: { x: 0.5, y: 0.5 } })
-        .rect(0, 0, 20, 20)
-        .fill(0xff00ff),
-    );
+    Assets.load("./img/test-image.jpg").then(image => {
+      this.pixiContainer.addChild(Sprite.from(image))
+      this.pixiContainer.zIndex = -999
+    })
   }
 }
