13c2d80c6e6450d44c27756ef355d8bb8c4ffb03
Kenos <flatware-gold-bronze@tutanota.com>
2024-04-19 11:30:57 -0600

    friction and velocity

--- src/objects/player.ts
+++ src/objects/player.ts
@@ -1,9 +1,11 @@
 import { Graphics } from "pixi.js";
 import { GameObject } from "../gameobject";
 import { GameScene } from "../scene";
+import { Vector, addVectors, createVector } from "../vector";
 
 export class Player extends GameObject {
   speed = 1000
+  velocity: Vector = createVector(0, 0)
   constructor(scene: GameScene) {
     super({x: 0, y: 0}, 0, scene)
     const graphics = new Graphics()
@@ -14,10 +16,13 @@ export class Player extends GameObject {
   }
 
   override act(delta: number): void {
-    if (this.scene.isKeyDown("a")) this.position.x -= delta * this.speed;
-    if (this.scene.isKeyDown("d")) this.position.x += delta * this.speed;
-    if (this.scene.isKeyDown("w")) this.position.y -= delta * this.speed;
-    if (this.scene.isKeyDown("s")) this.position.y += delta * this.speed;
+    if (this.scene.isKeyDown("a")) this.velocity.x = delta * -this.speed;
+    if (this.scene.isKeyDown("d")) this.velocity.x = delta * this.speed;
+    if (this.scene.isKeyDown("w")) this.velocity.y = delta * -this.speed;
+    if (this.scene.isKeyDown("s")) this.velocity.y = delta * this.speed;
+    this.velocity.x *= 0.9
+    this.velocity.y *= 0.9
+    this.position = addVectors(this.position, this.velocity)
     this.pixiContainer.x = this.position.x;
     this.pixiContainer.y = this.position.y;
 
